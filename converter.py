2e98b616ac002a02c5f5d8a449a941dea934e09b
bibysior <mail@mail.com>
2013-03-22 19:34:36 +0100

    poprawka

--- converter.py
+++ converter.py
@@ -11,11 +11,12 @@ def main():
 	drawer.draw(text)
 
 def getText():
-    if len(sys.argv) != 1:
+    if len(sys.argv) != 2:
         print "Bad arguments"
         sys.exit(1)
 
     return sys.argv[0]
+
 
 if __name__ == '__main__':
 	main()
