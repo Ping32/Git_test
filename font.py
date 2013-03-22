fbf7d6deb3413e402192d8a76c70339be06760a7
Ping32 <Ping32@wl.sk>
2013-03-22 18:55:58 +0100

    Czcionki wczytywane z plikow

--- font.py
+++ font.py
@@ -4,8 +4,13 @@ class Font:
 		self.letters = {}
 
 class FontLoader:
-	def loadFont(self, directory):
-		font = Font("fancy")
- 		return font
+    def loadFont(self, directory):
+        font = Font("fancy")
 
+        for filename in os.listdir(directory):
+            font.letters[filename] = readFile(directory+filename)
 
+        return font
+
+def readFile(filename):
+    return open(filename).read()
